f1084c3487bcea5ccec02562ab0f97f1cd48e400
DivyeshPanchalDP <divyesh.panchal.566@gmail.com>
2022-01-05 14:11:39 +0530

    removed static duplicates

--- home/urls.py
+++ home/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('education', views.education, name='education'),
     path('projects', views.projects, name='projects'),
-    #path('contact', views.contact, name='contact'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('contact', views.contact, name='contact'),
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     #path('contact', TemplateView.as_view(template_name="contact.html")),
 ]
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['portfolio-django-divyesh.herokuapp.com','127.0.0.1']
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
     'home',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
