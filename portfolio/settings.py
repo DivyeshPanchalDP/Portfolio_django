ea0f8dad84fc94a2e18cf5ec9182ac50bc4deed8
DivyeshPanchalDP <divyesh.panchal.566@gmail.com>
2022-03-30 14:19:04 +0530

    debug true

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g8#+mbcu8e+#c)4+@$^y2efg2x5p3!v0w$628q5u#$urb_3x6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio-django-divyesh.herokuapp.com','127.0.0.1']
 
